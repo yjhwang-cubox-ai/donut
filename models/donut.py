3281787b364403071b9817c549c2b23f4118a048
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2025-02-13 00:58:51 +0900

    chore: [model] remove redundant code

--- models/donut.py
+++ models/donut.py
@@ -22,7 +22,7 @@ class Donut(L.LightningModule):
         self.log("train_loss", loss)
         return loss
     
-    def validation_step(self, batch, batch_idx, dataset_idx=0):
+    def validation_step(self, batch, batch_idx):
         pixel_values, labels, answers = batch
         batch_size = pixel_values.shape[0]
         # we feed the prompt to the model
@@ -30,7 +30,7 @@ class Donut(L.LightningModule):
         
         outputs = self.model.generate(pixel_values,
                                     decoder_input_ids=decoder_input_ids,
-                                    max_length=self.config.max_length,
+                                    max_length=config.model.max_length,
                                     early_stopping=True,
                                     pad_token_id=self.processor.tokenizer.pad_token_id,
                                     eos_token_id=self.processor.tokenizer.eos_token_id,
@@ -53,7 +53,7 @@ class Donut(L.LightningModule):
             answer = answer.replace(self.processor.tokenizer.eos_token, "")
             scores.append(edit_distance(pred, answer) / max(len(pred), len(answer)))
 
-            if self.config.get("verbose", False) and len(scores) == 1:
+            if config.training.verbose==False and len(scores) == 1:
                 print(f"Prediction: {pred}")
                 print(f"    Answer: {answer}")
                 print(f" Normed ED: {scores[0]}")
@@ -63,4 +63,5 @@ class Donut(L.LightningModule):
         return scores 
     
     def configure_optimizers(self):
-        return
+        optimizer = torch.optim.Adam(self.parameters(), lr=config.training.lr)
+        return optimizer
--- models/model_setup.py
+++ models/model_setup.py
@@ -3,26 +3,26 @@ from configs.config import config
 
 def init_model_config():
     model_config = VisionEncoderDecoderConfig.from_pretrained(
-        config.model_name_or_path, cache_dir=config.cache_dir
+        config.model.model_name_or_path, cache_dir=config.model.cache_dir
     )
-    model_config.encoder.image_size = config.image_size
-    model_config.decoder.max_length = config.max_length
+    model_config.encoder.image_size = config.model.image_size
+    model_config.decoder.max_length = config.model.max_length
     return model_config
 
 def init_processor():
     """DonutProcessor 초기화 및 설정"""
     processor = DonutProcessor.from_pretrained(
-        config.model_name_or_path, cache_dir=config.cache_dir
+        config.model.model_name_or_path, cache_dir=config.model.cache_dir
     )
     # 이미지 프로세서의 size는 (width, height) 순서여야 함
-    processor.image_processor.size = config.image_size[::-1]
+    processor.image_processor.size = config.model.image_size[::-1]
     processor.image_processor.do_align_long_axis = False
     return processor
 
 def init_model(model_config, processor):
     """VisionEncoderDecoderModel 초기화 및 토큰 관련 설정"""
     model = VisionEncoderDecoderModel.from_pretrained(
-        config.model_name_or_path, config=model_config, cache_dir=config.cache_dir
+        config.model.model_name_or_path, config=model_config, cache_dir=config.model.cache_dir
     )
     model.config.pad_token_id = processor.tokenizer.pad_token_id
     model.config.decoder_start_token_id = processor.tokenizer.convert_tokens_to_ids(['<s_cord-v2>'])[0]

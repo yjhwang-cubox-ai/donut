feb1eacffca7e25ecad1cd60ee127bd00652aa0e
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2025-02-13 13:21:12 +0900

    Training: code test

--- train.py
+++ train.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 import torch
 import lightning as L
 from lightning.pytorch.loggers import WandbLogger
-from lightning.pytorch.callbacks import ModelCheckpoint
 from configs.config import config
 from models.donut import Donut
 from data.data_module import DonutDataset
+from callback.custom_callback import HFTopKModelCheckpoint
 
 def train():
     # 모델 모듈 준비
@@ -15,23 +15,24 @@ def train():
     dataset = DonutDataset(model=model.model, processor=model.processor)
     # logger 준비
     wandb_logger = WandbLogger(project=config.wandb.project, name=config.wandb.name)
+    # 모델 저장 경로
+    save_dir = f'logs/{config.wandb.project}/{config.wandb.name}'
     # Callback 준비
-    callbacks = [
-        ModelCheckpoint(
-            monitor='val_edit_distance',
-            mode='min',
-            save_top_k=3,
-            filename='{epoch}-{step}-{val_edit_distance:.3f}',
-            verbose=True
-        )
-    ]
+    checkpoint_callback = HFTopKModelCheckpoint(
+        save_dir=save_dir,
+        monitor='val_edit_distance',
+        mode='min',
+        save_top_k=3,
+        training_info=config.wandb.training_info
+    )
 
     trainer = L.Trainer(
+            max_epochs=config.training.max_epochs,
             accelerator="gpu",
             devices=1,
             num_nodes=1,
             logger=wandb_logger,
-            callbacks=callbacks
+            callbacks=[checkpoint_callback]
     )
 
     trainer.fit(model=model, datamodule=dataset)
